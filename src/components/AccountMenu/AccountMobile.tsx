3077790f40747e005da9159b7c7c4d431af59452
ntatoud <noe.tatoud@insa-rouen.fr>
2024-01-27 03:14:20 +0100

    style:profile picture fit cover

--- src/components/AccountMenu/AccountMobile.tsx
+++ src/components/AccountMenu/AccountMobile.tsx
@@ -42,7 +42,11 @@ const AccountMobileLogged = () => {
 						: ""
 				}
 			>
-				<AvatarImage src={getFileUrl(account?.profilePictureKey)} alt="PP" />
+				<AvatarImage
+					src={getFileUrl(account?.profilePictureKey)}
+					alt="PP"
+					className="object-cover"
+				/>
 				<AvatarFallback className="uppercase">
 					{account?.name[0] ?? account?.email[0] ?? <User2 />}
 				</AvatarFallback>
